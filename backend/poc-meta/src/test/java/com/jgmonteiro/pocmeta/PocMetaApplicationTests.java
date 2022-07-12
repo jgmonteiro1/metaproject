56d627304210bfd87c4f1ee3ccd99a3ba90d2570
jgmonteiro1 <jgmoreira15@gmail.com>
2022-07-11 21:07:28 -0300

    Setup inicial do projeto

---
+++ backend/poc-meta/src/test/java/com/jgmonteiro/pocmeta/PocMetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jgmonteiro.pocmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocMetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
