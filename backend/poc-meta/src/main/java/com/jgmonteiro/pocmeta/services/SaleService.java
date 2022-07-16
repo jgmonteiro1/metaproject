beaec98efca63528cac7bac3ddf0e9f803889b4d
jgmonteiro1 <jgmoreira15@gmail.com>
2022-07-16 03:20:52 -0300

    Insere regra de data minima e data maxima default

--- backend/poc-meta/src/main/java/com/jgmonteiro/pocmeta/services/SaleService.java
+++ backend/poc-meta/src/main/java/com/jgmonteiro/pocmeta/services/SaleService.java
@@ -7,7 +7,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
 import java.time.LocalDate;
+import java.time.ZoneId;
 
 @Service
 public class SaleService {
@@ -17,8 +19,10 @@ public class SaleService {
 
     public Page<Sale> findSales(final Pageable pageable, final String minDate, final String maxDate) {
 
-        final LocalDate min = LocalDate.parse(minDate);
-        final LocalDate max = LocalDate.parse(maxDate);
+        final LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+
+        final LocalDate min = maxDate.equals("") ? today.minusDays(365) : LocalDate.parse(minDate);
+        final LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
 
         return saleRepository.findSales(pageable, min, max);
     }
