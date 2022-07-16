2cc8de4f9c939907a25f1c78fc7cefc7c69ba62e
jgmonteiro1 <jgmoreira15@gmail.com>
2022-07-16 16:01:48 -0300

    Implementa notification com toastify

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { toast } from 'react-toastify';
 import icon from '../../assets/img/notification-icon.svg'
 import { BASE_URL } from '../../utils/request';
 
@@ -11,7 +12,7 @@ type Props = {
 function handleClick(saleId : number){
     axios(`${BASE_URL}/sales/notification/${saleId}`)
     .then(response =>{
-        console.log(`Enviou mensagem para o id: ${saleId}`)
+        toast.info(`Enviou mensagem para o id: ${saleId}`);
     })
 }
 
